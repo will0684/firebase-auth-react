634c4263a3af00aca5db84a76e15611cede7e086
will0684 <jordan.walcaraz@gmail.com>
2019-01-24 10:23:39 -0500

    Setup authorization "API"/Interface in Firebase class

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -25,6 +25,22 @@ const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
 class Firebase {
   constructor() {
     firebase.initializeApp(config);
+    // Get the auth instance
+    this.auth = firebase.auth();
+    // Auth User "API"/Interface
+    doCreateUserWithEmailAndPassword = (email, password) => {
+      this.auth.createUserWithEmailAndPassword(email, password);
+    }
+
+    doSignInWithEmailAndPassword = (email, password) => {
+      this.auth.signInWithEmailAndPassword(email, password);
+    }
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
   }
 }
 
