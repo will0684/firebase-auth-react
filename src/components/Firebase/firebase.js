d4fd1aa51d09d2acfc98849d978f94a9f2662224
will0684 <jordan.walcaraz@gmail.com>
2019-01-24 09:57:00 -0500

    Configured firebase and environment variables for firebase dev/prod config

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,29 @@
+import { app } from "firebase";
+
+var devConfig = {
+  apiKey: process.env.REACT_APP_DEV_API_KEY,
+  authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DEV_DATABASE_URL,
+  projectId: process.env.REACT_APP_DEV_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER
+};
+
+var prodConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER
+};
+
+const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+  }
+}
+
+export default Firebase;
